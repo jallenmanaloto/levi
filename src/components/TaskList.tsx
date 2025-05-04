1c1f03acca548e31970f42636353883dd770240f
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 20:00:06 +0800

    feat: integrate query on tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,26 +1,12 @@
-import { useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
-import { Task as TTask } from "../lib/types.ts";
 import Task from "./Task";
+import { useFetchTasks } from "../data/task.ts";
 
 export default function TaskList() {
-  const [tasks, setTasks] = useState<TTask[] | null>(null);
+  const { data: tasks } = useFetchTasks();
 
-  const fetchTasks = async () => {
-    try {
-      const newTask = await invoke<TTask[]>('get_tasks_command');
-      setTasks(newTask);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
   return (
     <>
-      {tasks !== null ? tasks.map((task, key: number) => {
+      {tasks ? tasks.map((task, key: number) => {
         return (
           <Task key={key} task={task} />
         )
