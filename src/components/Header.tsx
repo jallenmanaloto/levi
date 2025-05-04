8492e716bbe8305ea002c7c59a5896972d7c8078
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 13:36:34 +0800

    style: add header background and border radius

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import TaskFilter from "./TaskFilter";
 
 export default function Header() {
   return (
-    <div className="w-full h-12 flex">
+    <div className="w-full h-12 flex background-pill rounded-full">
       <TaskFilter />
     </div>
   )
