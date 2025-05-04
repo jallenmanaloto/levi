385bf3d10c2c998116b62d0d0af1b1a00b1023a3
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 13:53:53 +0800

    feat: add conditions on styles

--- src/components/TaskFilter.tsx
+++ src/components/TaskFilter.tsx
@@ -5,17 +5,17 @@ export default function TaskFilter() {
   const [activeCategory, setActiveCategory] = useState<string>('To do');
 
   return (
-    <div className="h-12 flex items-center">
+    <div className="h-12 flex items-center base-text">
       {categories.map((category: string, key: number) => {
         const isActive = activeCategory === category;
         return (
           <div
             onClick={() => setActiveCategory(category)}
-            className={`w-28 h-7 ml-4 flex justify-center items-center ${isActive ? 'bg-neutral-200/10' : 'bg-stone-800/40'} rounded-full cursor-pointer`}
+            className={`w-28 h-7 ml-4 base-text flex justify-center items-center ${isActive ? 'bg-neutral-200/10' : 'bg-stone-800/40'} rounded-full cursor-pointer`}
             key={key}
           >
-            <h2 className="text-xs text-gray-100 px-2">
-              {category}: <span className="text-green-200">(100)</span>
+            <h2 className={`${isActive ? 'text-gray-100' : ''} text-xs px-2`}>
+              {category}: <span className={`${isActive ? 'text-green-200' : 'text-green-200/20'}`}>(100)</span>
             </h2>
           </div>
         )
