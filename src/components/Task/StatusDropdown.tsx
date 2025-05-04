2e47ae63c686bea9334dcd8ce2a876e66e3fef8c
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 22:13:52 +0800

    feat: create status dropdown component

---
+++ src/components/Task/StatusDropdown.tsx
@@ -0,0 +1,41 @@
+import { Task, TaskStatus } from "../../lib/types"
+import { useUpdateTask } from "../../data/task";
+
+export default function StatusDropdown({ task, status, setStatus }: { task: Task, status: TaskStatus, setStatus: (status: TaskStatus) => void }) {
+  const statusLabels: Record<TaskStatus, string> = {
+    [TaskStatus.TODO]: "To do",
+    [TaskStatus.ONGOING]: "Ongoing",
+    [TaskStatus.DONE]: "Done"
+  };
+
+  const updateTask = useUpdateTask();
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newStatus = e.target.value as TaskStatus;
+    setStatus(newStatus);
+    updateTask.mutate({
+      id: task.id,
+      title: task.title,
+      status: newStatus
+    });
+  }
+
+  return (
+    <>
+      <select
+        className={`bg-transparent text-xs outline-none appearance-none cursor-pointer text-center
+        ${status === TaskStatus.TODO ? 'text-indigo-500/37'
+            : status === TaskStatus.ONGOING ? 'text-yellow-400/50'
+              : status === TaskStatus.DONE ? 'text-green-800'
+                : ''}`}
+        value={status}
+        onChange={handleChange}
+      >
+        {Object.entries(statusLabels).map(([value, label]) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </>
+  )
+}
