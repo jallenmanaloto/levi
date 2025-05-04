4bee9e575ee98986d1087bc43df3863cb0f7c0ab
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 16:59:11 +0800

    feat: add Task type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,10 +1,15 @@
+export type Task = {
+  name: string;
+  createdAt: string
+};
+
 export type TaskStyle = {
   background: string;
   border: string;
-}
+};
 
 export enum TaskStatus {
   TODO = 'To do',
   ONGOING = 'Ongoing',
   DONE = 'Done'
-}
+};
