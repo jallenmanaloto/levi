8b8d6d94f70920fbf6284e0f8967cfa147481a52
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 18:58:13 +0800

    feat: create main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,89 @@
+import { invoke } from "@tauri-apps/api/core";
+import { CircleChevronRight, Plus, X } from 'lucide-react';
+import Header from "../components/Header";
+import { useState } from 'react';
+import TaskList from "./TaskList";
+
+export default function Main() {
+  const [addTask, setAddTask] = useState(false);
+  const [taskTitle, setTaskTitle] = useState('');
+  const [taskStatus, setTaskStatus] = useState('todo');
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskTitle(event.target.value);
+  };
+
+  const handleAddTaskSubmit = async () => {
+    if (taskTitle.trim()) {
+      try {
+        const newTask = await invoke('add_task_command', { title: taskTitle, status: taskStatus });
+        console.log('Task added successfully:', newTask);
+        // Optionally update your task list in the UI here
+        setAddTask(false);
+        setTaskTitle('');
+        // You might want to reset the status as well if needed:
+        setTaskStatus('todo');
+      } catch (error) {
+        console.error('Failed to add task:', error);
+        // Display an error message to the user
+      }
+    } else {
+      // Optionally show a message to the user that the task title cannot be empty
+    }
+  };
+
+  return (
+    <div className="container flex flex-col h-screen p-2 background-main-dark">
+      <Header />
+      <div className="flex-1 py-4 px-3 pt-12 space-y-4 overflow-y-auto">
+        {/* List of tasks */}
+        <TaskList />
+      </div>
+
+
+      <div className="w-full py-6">
+        {addTask ? (
+          <div className="flex items-center w-full h-12 background-pill rounded-full transition-all duration-500 ease-in-out transform opacity-100 scale-100">
+            <div className="flex justify-center items-center rounded-full h-8 w-8 bg-stone-800/40 mx-2 cursor-pointer">
+              <X
+                onClick={() => setAddTask(false)}
+                className="text-green-200 h-4 w-4"
+              />
+            </div>
+            <input
+              type='text'
+              placeholder='Task name'
+              className="text-sm w-5/6 text-gray-100/30 flex-1 outline-none"
+              value={taskTitle}
+              onChange={handleTitleChange}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  handleAddTaskSubmit();
+                }
+              }}
+            />
+            <CircleChevronRight
+              onClick={handleAddTaskSubmit}
+              className="text-green-200 h-6 w-6 mr-3 cursor-pointer"
+            />
+          </div>
+        ) : (
+          <div
+            onClick={() => {
+              setAddTask(true)
+              setTaskTitle('');
+            }}
+            className="flex items-center space-x-2 h-12 px-2 text-center cursor-pointer w-32 background-pill rounded-full transition-all duration-300 ease-in-out opacity-100 scale-100"
+          >
+            <div className="flex justify-center items-center w-8 h-8 bg-green-300/50 rounded-full">
+              <Plus className="text-gray-100 h-6 w-6" />
+            </div>
+            <h2 className="text-sm text-green-300/50">
+              Add task
+            </h2>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
