0f0af75d50305aa3ee8a53c1b423e90a8f0763a2
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 20:59:18 +0800

    feat: set counts for each filters

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,25 @@
 import Task from "./Task";
 import { useFetchTasks } from "../data/task.ts";
+import { useFilterStore } from "../lib/store.ts";
+import { useEffect } from "react";
+import { TaskStatus } from "../lib/types.ts";
 
 export default function TaskList() {
+  const { setDoneCount, setOngoingCount, setTodoCount, setTotal } = useFilterStore();
   const { data: tasks } = useFetchTasks();
+
+  useEffect(() => {
+    if (tasks) {
+      const doneCount = tasks.filter(task => task.status === TaskStatus.DONE).length;
+      const ongoingCount = tasks.filter(task => task.status === TaskStatus.ONGOING).length;
+      const todoCount = tasks.filter(task => task.status === TaskStatus.TODO).length;
+
+      setTotal(tasks.length);
+      setTodoCount(todoCount);
+      setDoneCount(doneCount);
+      setOngoingCount(ongoingCount);
+    }
+  }, [tasks]);
 
   return (
     <>
