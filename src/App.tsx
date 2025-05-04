d010fe87050c99795197de2a35253a99d7b6bdfb
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 18:58:24 +0800

    refactor: remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-// import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
-import TaskList from "./components/TaskList";
+import Main from "./components/Main";
 
 function App() {
   return (
     <main className="w-screen h-screen background-main-dark">
-      <TaskList />
+      <Main />
     </main>
   );
 }
