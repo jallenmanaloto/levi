b7956e7bfada5071d8b9e716cc0fc74ab24feb89
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 11:56:29 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container bg-stone-800">
+      <h1 className="text-red-500">Welcome to Tauri + React</h1>
+    </main>
+  );
+}
+
+export default App;
