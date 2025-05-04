e5b202d578b82b69ea4793f9fb3d03177948a754
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 18:59:27 +0800

    feat: integrate task data

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,22 +1,17 @@
 import { useState } from "react";
-import { Task as TTask, TaskStatus } from "../../lib/types";
+import { Task as TTask } from "../../lib/types";
 import Notes from '../Notes';
 
 import Details from "./Details";
 import Status from "./Status";
 
-export default function Task({ status }: { status: TaskStatus }) {
+export default function Task({ task }: { task: TTask }) {
   const [visible, setVisible] = useState(false);
 
-  // Dummy task data
-  const task: TTask = {
-    name: "A task here",
-    createdAt: "Jan-01-2025"
-  }
   return (
     <>
       <div className="flex">
-        <Status status={status} />
+        <Status status={task.status} />
         <Details visible={visible} setVisible={setVisible} task={task} />
       </div>
       {visible ? <Notes /> : null}
