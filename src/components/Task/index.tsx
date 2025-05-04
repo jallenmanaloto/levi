5fd4a1803f8f10b43e0d45e80b65d53f0326234b
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 16:17:31 +0800

    feat: handle notes for task expand

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { TaskStatus } from "../../lib/types";
+import Notes from '../Notes';
 
 import Details from "./Details";
 import Status from "./Status";
 
 export default function Task({ status }: { status: TaskStatus }) {
+  const [visible, setVisible] = useState(false);
   return (
-    <div className="flex">
-      <Status status={status} />
-      <Details />
-    </div>
+    <>
+      <div className="flex">
+        <Status status={status} />
+        <Details visible={visible} setVisible={setVisible} />
+      </div>
+      {visible ? <Notes /> : null}
+    </>
   )
 }
