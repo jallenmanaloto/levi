b46946d93290c7c58f2b190b6da4c2f621c860ea
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 13:54:01 +0800

    feat: integrate Voice component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import TaskFilter from "./TaskFilter";
+import Voice from "./Voice";
 
 export default function Header() {
   return (
-    <div className="w-full h-12 flex background-pill rounded-full">
+    <div className="w-full h-12 flex justify-between items-center background-pill rounded-full">
       <TaskFilter />
+      <Voice />
     </div>
   )
 }
