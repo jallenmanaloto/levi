e1d3226f7bfe322790d7cbee90ab14c030fa9236
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 17:07:11 +0800

    feat: add 'voice' text

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,10 @@ export default function Header() {
   return (
     <div className="w-full h-12 flex justify-between items-center background-pill rounded-full">
       <TaskFilter />
-      <Voice />
+      <div className="flex items-center space-x-3">
+        <Voice />
+        <h2 className="text-xs text-gray-100/20 pr-5">Voice</h2>
+      </div>
     </div>
   )
 }
