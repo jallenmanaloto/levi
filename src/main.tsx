aadb41d698c9658a1cfa37ca0716440eb7972e9f
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 19:59:44 +0800

    feat: wrap app with query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+
+const queryClientProvider = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClientProvider}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
