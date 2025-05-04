4b99d67cfb7ce574dc8c81052c0314ce18566c45
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-04 13:53:40 +0800

    feat: create Voice component for activation

---
+++ src/components/Voice.tsx
@@ -0,0 +1,22 @@
+import { Mic, MicOff } from 'lucide-react';
+import { useState } from 'react';
+
+export default function Voice() {
+  const [active, setActive] = useState(false);
+  return (
+    <>
+      {active
+        ? <Mic
+          className="text-green-200 cursor-pointer mr-5"
+          size={18}
+          onClick={() => setActive(!active)}
+        />
+        : <MicOff
+          className="text-green-200 cursor-pointer mr-5"
+          size={18}
+          onClick={() => setActive(!active)}
+        />
+      }
+    </>
+  )
+}
