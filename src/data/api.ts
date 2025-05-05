37bb507f63d5739607a4dd8261e74f3352869203
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-05 20:39:57 +0800

    feat: add methods for notes

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,5 +1,5 @@
 import { invoke } from "@tauri-apps/api/core";
-import { Task, TaskStatus } from "../lib/types";
+import { Note, Task, TaskStatus } from "../lib/types";
 
 export const fetchTasks = async (): Promise<Task[]> => {
   return await invoke<Task[]>('get_tasks_command');
@@ -15,4 +15,12 @@ export const updateTask = async ({ id, title, status }: { id: number, title: str
 
 export const deletTask = async ({ id }: { id: number }): Promise<void> => {
   return await invoke('delete_task_command', { id });
+}
+
+export const addNote = async ({ note, taskId }: { note: string, taskId: number }): Promise<void> => {
+  return await invoke('add_note_command', { note, taskId: taskId });
+}
+
+export const fetchNotes = async ({ taskId }: { taskId: number }): Promise<Note[]> => {
+  return await invoke<Note[]>('get_notes_command', { taskId });
 }
