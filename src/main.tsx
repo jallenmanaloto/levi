a94110a9bcf3e5c6026804eecb9e2ab2608b743e
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-06 19:41:19 +0800

    feat: set path routes for voice and app

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+import VoiceWindow from "./components/Windows/VoiceWindow";
 
 const queryClientProvider = new QueryClient();
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/voice',
+    element: <VoiceWindow />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClientProvider}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
