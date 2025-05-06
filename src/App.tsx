1c109672d3a21fa538ef33a7ed294bf235a139b1
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-06 16:56:36 +0800

    feat: add updater to check for updates and install it

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
-import "./App.css";
-import Main from "./components/Main";
+import { useEffect } from 'react';
 import { Toaster } from "sonner";
+import { check } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process';
+
+import Main from "./components/Main";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const checkForUpdate = async () => {
+      const update = await check();
+      if (update) {
+        let downloaded = 0;
+        let contentLength = 0;
+
+        await update.downloadAndInstall((event) => {
+          switch (event.event) {
+            case 'Started':
+              contentLength = event.data.contentLength as number;
+              break;
+            case 'Progress':
+              downloaded += event.data.chunkLength;
+              break;
+            case 'Finished':
+              break;
+          }
+        });
+
+        await relaunch();
+      }
+    }
+
+    checkForUpdate();
+
+  }, [])
   return (
     <main className="w-screen h-screen background-main-dark">
       <Toaster richColors position="bottom-center" closeButton />
