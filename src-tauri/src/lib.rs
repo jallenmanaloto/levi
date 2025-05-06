037fe9b172a7cea886a57f5147438d063d8bf654
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-06 16:26:38 +0800

    feat: add autoupdater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             greet,
             add_task_command,
