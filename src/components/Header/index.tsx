1dfaf89fd1d7913fe8c8ad1f73867348dd4aadc4
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-06 13:50:13 +0800

    feat: integrate search and disable voice

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
+import Find from "../Search/index.tsx";
 import TaskFilter from "./TaskFilter.tsx"
-import Voice from "./Voice";
 
 export default function Header() {
   return (
     <div className="w-full h-12 flex justify-between items-center background-pill rounded-full">
       <TaskFilter />
       <div className="flex items-center space-x-3">
+        {/*
         <Voice />
         <h2 className="text-xs text-gray-100/20 pr-5">Voice</h2>
+        */}
+        <Find />
       </div>
     </div>
   )
