373ea025178129de33e3a9c029133d70fee8d12d
alleeeeenm <jallen.manaloto@gmail.com>
2025-05-06 20:06:53 +0800

    feat: add drag to voice window

--- src/components/Windows/VoiceWindow.tsx
+++ src/components/Windows/VoiceWindow.tsx
@@ -1,6 +1,6 @@
 export default function VoiceWindow() {
   return (
-    <div>
+    <div data-tauri-drag-region className="h-screen w-screen background-main-dark">
       <h2>Mini Window</h2>
       <p>This is a small window created with Tauri v2!</p>
     </div>
